4cfb1a8c9fd177f65cfe06d8b1ac6629b89ca1d8
Aditya Nemade <adityanemade@gmail.com>
2018-03-03 10:53:49 -0800

    added innerHTML

--- lab3/client/src/views/counter.js
+++ lab3/client/src/views/counter.js
@@ -5,12 +5,15 @@ export default function (store) {
 			this.store = store;
 			// TODO: render counter inner HTML based on the store state
 
+			this.innerHTML = "<h2>Hits:</h2><h2 id=\"resource-counter\" class=\"resource-counter\">" + this.store.state.counter + "</h2>";
+
 			this.onStateChange = this.handleStateChange.bind(this);
 		}
 
 		handleStateChange (newState) {
-			console.log('CounterComponent#stateChange', this, newState);
+			// console.log('CounterComponent#stateChange', this, newState);
 			// TODO: update inner HTML based on the new state
+			document.getElementById('resource-counter').innerHTML = this.store.state.counter;
 		}
 
 		connectedCallback () {
