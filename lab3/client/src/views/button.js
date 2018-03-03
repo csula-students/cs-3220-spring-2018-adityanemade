678e7eb385081abde3c235137a398b9d66ec91a4
Aditya Nemade <adityanemade@gmail.com>
2018-03-03 10:53:10 -0800

    added dispatch for button

--- lab3/client/src/views/button.js
+++ lab3/client/src/views/button.js
@@ -1,13 +1,24 @@
+import constants from '../constants';
+
 export default function (store) {
 	return class ButtonComponent extends window.HTMLElement {
 		constructor () {
 			super();
 			this.store = store;
 
-			this.onStateChange = this.handleStateChange.bind(this);
+			/*
+			 * Generating action button
+			 */
+			this.innerHTML = "<button id =\"btn-res\">Slaps</button>";
 
 			// TODO: add click event to increment counter
 			// hint: use "store.dispatch" method (see example component)
+			this.addEventListener('click', () => {
+				this.store.dispatch({
+					type: constants.actions.INCREMENT,
+					payload: window.globalGeneratorRate + 1
+				});
+			});
 		}
 	};
 }
