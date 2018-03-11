ca1903b97cb7cc7fdb20d6e04d0aa52114a79d61
Aditya Nemade <adityanemade@gmail.com>
2018-03-11 15:46:04 -0700

    rendered and handled state change

--- homework1/client/src/views/story-book.js
+++ homework1/client/src/views/story-book.js
@@ -1,3 +1,6 @@
+import constants from '../constants';
+import Story from '../models/story';
+
 export default function (store) {
 	return class StoryBookComponent extends window.HTMLElement {
 		constructor () {
@@ -8,12 +11,35 @@ export default function (store) {
 
 		handleStateChange (newState) {
 			// TODO: display story based on the state "resource" and "stories"
-			
+			// console.log(newState.story);
+			newState.story.forEach((s) => {
+				if(s.state == "visible"){
+					document.getElementById(s.name).style.visibility = s.state;
+					// console.log(document.getElementById(s.name));
+				}
+			});
 		}
 
 		connectedCallback () {
 			// this.store.subscribe(this.onStateChange);
-			this.innerHTML = "<div class = \"game-header\"><header><h1>Stress Reliever</h1></header></div><div class = \"game-story\"></div>";
+			this.innerHTML = "<div class = \"game-header\"><header><h1>Stress Reliever</h1></header></div>";
+			const storyParent = window.document.createElement('div');
+			storyParent.className = "game-story";
+			this.store.state.story.forEach((s) => {
+				const sModel = new Story(s);
+				const storyPara = window.document.createElement('span');
+				storyPara.id = s.name;
+				storyPara.innerHTML = s.description;
+				storyPara.style.visibility = s.state;
+				const storyBreak = window.document.createElement('br');
+				storyPara.appendChild(storyBreak);
+				storyParent.appendChild(storyPara);
+			});
+			this.append(storyParent);
+
+
+			this.store.subscribe(this.onStateChange);
+
 		}
 
 		disconnectedCallback () {
